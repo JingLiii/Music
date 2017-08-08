4c63b18acdf777952b13c3b8f5452abfb1f9f24c
zhangrh <zhangrhweb@163.com>
2017-08-08 16:16:51 +0800

    header组件的基本搭建

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
   <div id="app">
+    <m-header></m-header>
     Hello World
   </div>
 </template>
 
 <script>
+import MHeader from 'components/m-header/m-header'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    MHeader
+  }
 }
 </script>
+
+<style lang="stylus" scoped rel="stylesheet/stylus">
+@import "~common/stylus/variable.stylus"
+
+#app
+  color: $color-theme
+</style>
+
 
 
