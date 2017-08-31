767e0631a06dc051963e2d457d55bf6926180c86
zhangrh <zhangrhweb@163.com>
2017-08-31 11:57:35 +0800

    重新写了music-list组件,  发现了bg-layer标签写错了位置, 有两点没有想明白, 首先是关于偏移量的问题, 再就是关于ba-layer标签的作用

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -31,22 +31,37 @@ export default {
 <style lang="stylus" scoped>
   @import "~common/stylus/variable.stylus"
   @import "~common/stylus/mixin.stylus"
+
   .song-list
     .item
-      display flex
-      align-items center
-      box-sizing border-box
-      height 64px
-      font-size $font-size-medium
+      display: flex
+      align-items: center
+      box-sizing: border-box
+      height: 64px
+      font-size: $font-size-medium
+      .rank
+        flex: 0 0 25px
+        width: 25px
+        margin-right: 30px
+        text-align: center
+        .icon
+          display: inline-block
+          width: 25px
+          height: 24px
+          background-size: 25px 24px
+        .text
+          color: $color-theme
+          font-size: $font-size-large
       .content
-        flex 1
-        line-height 20px
-        overflow hidden
+        flex: 1
+        line-height: 20px
+        overflow: hidden
         .name
           no-wrap()
-          color $color-text
+          color: $color-text
         .desc
           no-wrap()
-          color $color-text-d
+          margin-top: 4px
+          color: $color-text-d
 </style>
 
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -3,11 +3,14 @@
     <div class="back">
       <i class="icon-back"></i>
     </div>
+    <!-- 因为其中有html的转义字符 -->
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
       <div class="filter"></div>
     </div>
-    <scroll :data="songs" class="list" ref="list">
+    <div class="bg-layer" ref="layer"></div>
+    <!-- 实时监听scroll的位置 -->
+    <scroll @scroll="scroll" :probe-type="probeType" :listen-scroll="listenScroll" :data="songs" class="list" ref="list">
       <!-- 套一层, 为了控制内部的样式 -->
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
@@ -20,11 +23,16 @@
   import Scroll from 'base/scroll/scroll'
   import SongList from 'base/song-list/song-list'
 
+  // 预留的一个常量, 具体顶部保留的一个距离
+  const RESERVEL_HEIGHT = 40
+
   export default {
+    // 组件
     components: {
       Scroll,
       SongList
     },
+    // 传进来的属性
     props: {
       // 背景图
       bgImage: {
@@ -42,14 +50,53 @@
         default: ''
       }
     },
+    // 计算属性
     computed: {
       bgStyle() {
         return `background-image: url(${this.bgImage})`
       }
     },
+    // 数据
+    data () {
+      return {
+        // 监听纵向滚动的值
+        scrollY: 0
+      }
+    },
+    // 方法
+    methods: {
+      scroll(pos) {
+        // 实时监听并更改scrollY的值
+        this.scrollY = pos.y
+      }
+    },
+    watch: {
+      // 然后根据scrollY的值, 设置一个bg-layer的一个偏移量
+      scrollY(newY) {
+        // 无论我们如何拖动bg-layer始终都在那, max可以计算出来具体的偏移量
+        let translateY = Math.max(this.minTranslateY, newY)
+        this.$refs.layer.style[`transform`] = `translate3d(0, ${translateY}px, 0)`
+        this.$refs.layer.style[`webkitTransform`] = `translate3d(0, ${translateY}px, 0)`
+      }
+    },
+    // 创建整个Dom的时候
+    created () {
+      // 控制scroll组件的属性
+      // 1: 在滑动停止时, 派发scroll事件
+      // 2: 在滑动过程中派发scroll事件
+      // 3: 不仅在滑动过程中, 也在momentum滚动动画时派发.
+      this.probeType = 3
+      // 是否监听滚动事件
+      this.listenScroll = true
+    },
+    // 实例化对象的时候
     mounted () {
+      // 记录下图片的高度
+      this.imageHeight = this.$refs.bgImage.clientHeight
+      // 记录下最小滚动的值
+      this.minTranslateY = -this.imageHeight + RESERVEL_HEIGHT
       // 计算距离上不的top值的大小
-      this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`
+      this.$refs.list.$el.style.top = `${this.imageHeight}px`
     }
   }
 </script>
@@ -58,87 +105,89 @@
   @import "~common/stylus/variable.stylus"
   @import "~common/stylus/mixin.stylus"
   .music-list
-    position fixed
-    top 0
-    left 0
-    bottom 0
-    right 0
-    background $color-background
+    position: fixed
+    z-index: 100
+    top: 0
+    left: 0
+    bottom: 0
+    right: 0
+    background: $color-background
     .back
       position absolute
-      top 0
-      left 6px
-      z-index 5
+      top: 0
+      left: 6px
+      z-index: 50
       .icon-back
-        display block
-        padding 10px
-        font-size $font-size-large-x
-        color $color-theme
+        display: block
+        padding: 10px
+        font-size: $font-size-large-x
+        color: $color-theme
     .title
-      position absolute
-      top 0
-      left 10%
-      z-index 4
-      width 80%
+      position: absolute
+      top: 0
+      left: 10%
+      z-index: 40
+      width: 80%
       no-wrap()
-      text-align center
-      line-height 40px
-      font-size $font-size-large
-      color $color-text
+      text-align: center
+      line-height: 40px
+      font-size: $font-size-large
+      color: $color-text
     .bg-image
-      position relative
-      width 100%
-      height 0
-      padding-top 70%
-      z-index 5
-      transform-origin top
-      background-size cover
+      position: relative
+      width: 100%
+      height: 0
+      padding-top: 70%
+      transform-origin: top
+      background-size: cover
       .play-wrapper
-        position absolute
-        bottom 20px
-        z-index 5
-        width 100%
+        position: absolute
+        bottom: 20px
+        z-index: 50
+        width: 100%
         .play
-          box-sizing border-box
-          width 135px
-          padding 7px 0
-          margin 0 auto
-          text-align center
-          border 1px solid $color-theme
-          color $color-theme
-          border-radius 100px
-          font-size 0
+          box-sizing: border-box
+          width: 135px
+          padding: 7px 0
+          margin: 0 auto
+          text-align: center
+          border: 1px solid $color-theme
+          color: $color-theme
+          border-radius: 100px
+          font-size: 0
           .icon-play
-            display inline-block
-            vertical-align middle
-            margin-right 6px
-            font-size $font-size-samll
+            display: inline-block
+            vertical-align: middle
+            margin-right: 6px
+            font-size: $font-size-medium-x
           .text
-            display inline-block
-            vertical-algin middle
-            font-size $font-size-small
+            display: inline-block
+            vertical-align: middle
+            font-size: $font-size-small
       .filter
-        position absolute
-        top 0
-        left 0
-        width 100%
-        height 100%
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
         background: rgba(7, 17, 27, 0.4)
     .bg-layer
-      position relative
-      height 100%
-      background $color-background
+      position: relative
+      height: 100%
+      background: $color-background
     .list
-      position fixed
-      top 0
-      bottom 0
-      width 100%
-      background $color-background
+      position: fixed
+      top: 0
+      bottom: 0
+      width: 100%
+      // 去掉之后, 上滑就不会被遮盖
+      // overflow hidden
+      background: $color-background
       .song-list-wrapper
-        padding 20px 30px
+        padding: 20px 30px
       .loading-container
-        position absolute
-        width 100%
-        top 50%
-        transform translateY(-50%)
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
 </style>
