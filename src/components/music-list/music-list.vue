26691c3053dd0a8ccc9cb1814cd100479b49ce0b
zhangrh <zhangrhweb@163.com>
2017-09-12 10:41:49 +0800

    完成直接随机播放某列表

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -7,7 +7,7 @@
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
       <div class="play-wrapper">
-        <div class="play" v-show="songs.length>0" ref="playBtn">
+        <div class="play" v-show="songs.length>0" @click="random" ref="playBtn">
           <i class="icon-play"></i>
           <span class="text">随机播放</span>
         </div>
@@ -102,9 +102,16 @@
           index
         })
       },
+      // 随机播放
+      random() {
+        this.palyRandomList({
+          list: this.songs
+        })
+      },
       // 用来获取actions中暴露出的方法
       ...mapActions([
-        'selectPlay'
+        'selectPlay',
+        'palyRandomList'
       ])
     },
     watch: {
--- src/store/actions.js
+++ src/store/actions.js
@@ -3,6 +3,8 @@
 // 一次修改, 多个mutation
 
 import * as types from './mutation-types'
+import {playMode} from 'common/js/config'
+import {shuffle} from 'common/js/until'
 
 // 选择了歌曲, 然后需要更改一系列的状态
 export const selectPlay = function ({commit, state}, {list, index}) {
@@ -12,6 +14,23 @@ export const selectPlay = function ({commit, state}, {list, index}) {
   commit(types.SET_PLAYLIST, list)
   // 更改播放序号, 也就是当前播放的哪一个
   commit(types.SET_CURRENT_INDEX, index)
+  // 更改全篇显示状态
+  commit(types.SET_FULL_SCREEN, true)
+  // 更改播放状态
+  commit(types.SET_PLAYING_STATE, true)
+}
+
+// 开始随机播放某个歌单
+export const palyRandomList = function ({commit}, {list}) {
+  // 设置播放状态
+  commit(types.SET_PLAY_MODE, playMode.random)
+  // 设置一个顺便播放列表
+  commit(types.SET_SEQUENCE_LIST, list)
+  let randomList = shuffle(list)
+  // 更改播放列表
+  commit(types.SET_PLAYLIST, randomList)
+  // 从随机列表的第一个开始播放
+  commit(types.SET_CURRENT_INDEX, 0)
   // 更改全篇显示状态
   commit(types.SET_FULL_SCREEN, true)
   // 更改播放状态
